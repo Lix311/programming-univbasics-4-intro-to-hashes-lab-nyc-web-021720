826a3874e82968656538d5f7a62ef966f6a1072a
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-04 22:36:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,15 +10,17 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  
+  person = {:name => "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  number = {:id => 2}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  my_hash = {:#{key} => }
 end
 
 def read_from_hash(hash, key)
